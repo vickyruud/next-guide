bb5efac1f7319c6d9e25be76338acd5d24ad7253
vickyruud <aprajit.k@gmail.com>
2022-07-29 21:16:26 -0400

    render character page from server

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-  This is from _app.tsx
-  <Component {...pageProps} /></>
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -1,9 +1,13 @@
-import React from "react";
-import { Character, GetCharacterResults } from "../../types";
+import { GetServerSideProps } from "next";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import imageLoader from "../../imageLoader";
+import { Character } from "../../types";
 
 const CharacterPage = ({ character }: { character: Character }) => {
+  const router = useRouter();
+  console.log(router.query.id);
+
   return (
     <div>
       <h1>{character.name}</h1>
@@ -19,23 +23,10 @@ const CharacterPage = ({ character }: { character: Character }) => {
   );
 };
 
-export async function getStaticPaths() {
-  const res = await fetch("https://rickandmortyapi.com/api/character");
-  const { results }: GetCharacterResults = await res.json();
-
-  return {
-    paths: results.map((character) => {
-      return { params: { id: String(character.id) } };
-    }),
-    fallback: false,
-  };
-}
-
-export async function getStaticProps({ params }: { params: { id: string } }) {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const res = await fetch(
-    `https://rickandmortyapi.com/api/character/${params.id}`
+    `https://rickandmortyapi.com/api/character/${context.query.id}`
   );
-
   const character = await res.json();
 
   return {
@@ -43,5 +34,5 @@ export async function getStaticProps({ params }: { params: { id: string } }) {
       character,
     },
   };
-}
+};
 export default CharacterPage;
